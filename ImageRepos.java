a3c7a0fdf31a837d89d10003b6b09c234aa7889d
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-10 18:47:48 +0300

    Delete ImageRepos.java

--- ImageRepos.java
+++
@@ -1,8 +0,0 @@
-package com.example.dishes.repository;
-
-import com.example.dishes.entity.ImageEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ImageRepos extends CrudRepository<ImageEntity, Long> {
-    ImageEntity findByPicture(String picture);
-}
