2d0947c4c35909a036b67558196bd007fecae758
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-10 18:34:16 +0300

    Update DishRepos.java

--- src/main/java/com/example/dishes/repository/DishRepos.java
+++ src/main/java/com/example/dishes/repository/DishRepos.java
@@ -3,8 +3,6 @@ package com.example.dishes.repository;
 import com.example.dishes.entity.DishEntity;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface DishRepos extends CrudRepository<DishEntity, Long> {
-    List<DishEntity> findByName(String name);
+    DishEntity findByName(String name);
 }
