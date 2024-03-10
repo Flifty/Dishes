765a491367aaf15bea959560ac234def2ccfbef1
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-10 18:48:56 +0300

    Delete IngredientRepos.java

--- IngredientRepos.java
+++
@@ -1,8 +0,0 @@
-package com.example.dishes.repository;
-
-import com.example.dishes.entity.IngredientEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IngredientRepos extends CrudRepository<IngredientEntity, Long> {
-    IngredientEntity findByName(String name);
-}
