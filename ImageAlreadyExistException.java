28e2ac66a51ddbebabc078a553f1818290d2f6d7
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-10 18:47:25 +0300

    Delete ImageAlreadyExistException.java

--- ImageAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.dishes.exception;
-
-public class ImageAlreadyExistException extends Exception {
-    public ImageAlreadyExistException(String message) {
-        super(message);
-    }
-}
