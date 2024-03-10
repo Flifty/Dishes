2a26585aa0bdfe7892925809c187cab926dee4e7
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-10 18:48:25 +0300

    Delete IngredientAlreadyExistException.java

--- IngredientAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.dishes.exception;
-
-public class IngredientAlreadyExistException extends Exception {
-    public IngredientAlreadyExistException(String message) {
-        super(message);
-    }
-}
