24b00a7f6a9565337001090e4d2c9f8a19514902
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-10 18:31:32 +0300

    Update DishController.java

--- src/main/java/com/example/dishes/controller/DishController.java
+++ src/main/java/com/example/dishes/controller/DishController.java
@@ -1,25 +1,21 @@
 package com.example.dishes.controller;
 
 import com.example.dishes.entity.DishEntity;
-import com.example.dishes.exception.DishAlreadyExistExeption;
+import com.example.dishes.exception.DishAlreadyExistException;
 import com.example.dishes.exception.DishNotFoundException;
 import com.example.dishes.model.Dish;
 import com.example.dishes.service.DishService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/dishes")
 public class DishController {
 
-    @Value("${dishbit.api-key}")
-    private String apiKey;
-
     private static final String ERROR_MESSAGE = "Произошла ошибка";
-
     private final DishService dishService;
 
     public DishController(DishService dishService) {
@@ -27,11 +23,11 @@ public class DishController {
     }
 
     @PostMapping
-    public ResponseEntity addDish(@RequestBody DishEntity dish) {
+    public ResponseEntity<?> addDish(@RequestBody DishEntity dish) {
         try {
-            dishService.add(dish);
+            dishService.addDish(dish);
             return ResponseEntity.ok("Блюдо было успешно сохранено");
-        } catch (DishAlreadyExistExeption e) {
+        } catch (DishAlreadyExistException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(ERROR_MESSAGE);
@@ -39,9 +35,9 @@ public class DishController {
     }
 
     @GetMapping
-    public ResponseEntity getDishFromDb(@RequestParam(required = false) String name) {
+    public ResponseEntity<?> getDish(@RequestParam(required = false) String name) {
         try {
-            return ResponseEntity.ok(dishService.getFromDb(name));
+            return ResponseEntity.ok(dishService.getDish(name));
         } catch (DishNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
@@ -61,14 +57,27 @@ public class DishController {
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteDish(@PathVariable Long id) {
+    @PutMapping
+    public ResponseEntity<?> updateDish(@RequestParam String name, @RequestBody DishEntity updatedDish) {
         try {
-            dishService.delete(id);
-            return ResponseEntity.ok("Блюдо было успешно удалено");
+            dishService.updateDish(name, updatedDish);
+            return ResponseEntity.ok("Блюдо было успешно изменено");
+        } catch (DishNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(ERROR_MESSAGE);
         }
     }
 
+    @DeleteMapping
+    public ResponseEntity<?> deleteDish(@RequestParam Long id) {
+        try {
+            dishService.deleteDish(id);
+            return ResponseEntity.ok("Блюдо было успешно удалено");
+        } catch (DishNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ERROR_MESSAGE);
+        }
+    }
 }
