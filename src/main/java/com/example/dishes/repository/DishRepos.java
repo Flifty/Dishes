6f1ba91cd81604827c2d9744fbf6400e9cb0e1ae
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-17 17:13:54 +0300

    Delete src/main/java/com/example/dishes/repository/DishRepos.java

--- src/main/java/com/example/dishes/repository/DishRepos.java
+++
@@ -1,8 +0,0 @@
-package com.example.dishes.repository;
-
-import com.example.dishes.entity.DishEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DishRepos extends CrudRepository<DishEntity, Long> {
-    DishEntity findByName(String name);
-}
