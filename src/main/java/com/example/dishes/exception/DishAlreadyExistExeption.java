c6aab9c649f0bfc1107559eb07ef9b1030dea4b0
Flifty <112774881+Flifty@users.noreply.github.com>
2024-03-17 17:14:41 +0300

    Delete src/main/java/com/example/dishes/exception/DishAlreadyExistExeption.java

--- src/main/java/com/example/dishes/exception/DishAlreadyExistExeption.java
+++
@@ -1,7 +0,0 @@
-package com.example.dishes.exception;
-
-public class DishAlreadyExistExeption extends Exception {
-    public DishAlreadyExistExeption(String message) {
-        super(message);
-    }
-}
