41bf631b3d942a484510bc9f0918d1991f73ff3b
Flifty <112774881+Flifty@users.noreply.github.com>
2024-05-23 00:07:17 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,24 +1,24 @@
-CREATE TABLE dish (                          
+CREATE TABLE dish (
                       id SERIAL PRIMARY KEY,
-                      name VARCHAR(255) NOT NULL,                      
+                      name VARCHAR(255) NOT NULL,
                       country VARCHAR(255),
-                      category VARCHAR(255),                      
+                      category VARCHAR(255),
                       instruction TEXT
 );
 
-CREATE TABLE image (   
+CREATE TABLE image (
                        id SERIAL PRIMARY KEY,
-                       picture VARCHAR(255) NOT NULL, 
+                       picture VARCHAR(255) NOT NULL,
                        dish_id BIGINT REFERENCES dish(id)
 );
 
-CREATE TABLE ingredient (                            
+CREATE TABLE ingredient (
                             id SERIAL PRIMARY KEY,
                             name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE dish_ingredient_mapping (
-                                         dish_id BIGINT REFERENCES dish(id),                                         
+                                         dish_id BIGINT REFERENCES dish(id),
                                          ingredient_id BIGINT REFERENCES ingredient(id),
                                          PRIMARY KEY (dish_id, ingredient_id)
 );
